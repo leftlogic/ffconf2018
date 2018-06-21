327bdaac5458f66efc4297aa51cfb2cb6b9c6fbf
Giulia Alfonsi <electric.g@gmail.com>
2018-06-21 11:52:37 -0700

    Fixed doy curtains position

--- src/components/decorations/duke-of-yorks/index.js
+++ src/components/decorations/duke-of-yorks/index.js
@@ -3,7 +3,7 @@ import './duke-of-yorks.scss';
 const DukeOfYorks = () => {
   return (
     <div className="duke-of-yorks">
-      <div className="curtains" />
+      <div className="duke-of-yorks__curtains" />
     </div>
   );
 };
